9d9f5cb4bea523f0b6babfddf05f661d16c18fa6
unknown <mikeman@inbox.ru>
2021-12-01 10:28:04 +0300

    added all pages

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   },
   computed: {
     layout() {
-      return (this.$route.meta.layout || 'empty')
+      return (this.$route.meta.layout || 'empty') + '-layout'
     }
   }
     
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,64 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    meta: {layout: 'main'},
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/login',
-    name: 'Login',
+    name: 'login',
     meta: {layout: 'empty'},
     component: () => import('../views/Login.vue')
   },
   {
     path: '/categories',
-    name: 'Categories',
-    meta: {layout: 'MainLayout'},
+    name: 'categories',
+    meta: {layout: 'main'},
     component: () => import('../views/Categories.vue')
-  }
+  },
+  {
+    path: '/detailrecord',
+    name: 'detailrecord',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planing',
+    name: 'planing',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planing.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue')
+  },
 
 ]
 
